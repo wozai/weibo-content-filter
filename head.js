0cf7a643d6081aa1ba8444bfaeb065ab4a03686a
Fang Zhang <zhangfang.thu@gmail.com>
2013-04-06 16:43:15 +0800

    compile: ???????? build: ?????????? ??????????

---
+++ head.js
@@ -0,0 +1,19 @@
+// ==UserScript==
+// @name			眼不见心不烦（新浪微博）
+// @namespace		http://weibo.com/salviati
+// @license			MIT License
+// @description		新浪微博（weibo.com）非官方功能增强脚本，具有屏蔽关键词、用户、来源、链接，改造版面等功能
+// @features		停止对旧版微博的支持；修正屏蔽来源在用户主页失效的问题；修正更换模板功能在使用非自定义模板的页面失效的问题；首次进入极简阅读模式时显示提示
+// @version			${MAINVER}
+// @revision		${REV}
+// @author			@富平侯
+// @committers		@牛肉火箭, @JoyerHuang_悦
+// @grant			GM_getValue
+// @grant			GM_setValue
+// @grant			GM_addStyle
+// @grant			GM_xmlhttpRequest
+// @include			http://weibo.com/*
+// @include			http://www.weibo.com/*
+// @updateURL		https://userscripts.org/scripts/source/114087.meta.js
+// @downloadURL		https://userscripts.org/scripts/source/114087.user.js
+// ==/UserScript==
--- weiboFilter.user.js
+++ weiboFilter.user.js
@@ -1,25 +1,3 @@
-// ==UserScript==
-// @name			眼不见心不烦（新浪微博）
-// @namespace		http://weibo.com/salviati
-// @license			MIT License
-// @description		新浪微博（weibo.com）非官方功能增强脚本，具有屏蔽关键词、用户、来源、链接，改造版面等功能
-// @features		【！！旧版微博用户请勿升级！！】；修正屏蔽来源在用户主页失效的问题；修正更换模板功能在使用非自定义模板的页面失效的问题；首次进入极简阅读模式时显示提示
-// @version			1.1
-// @revision		76
-// @author			@富平侯
-// @committers		@牛肉火箭, @JoyerHuang_悦
-// @grant			GM_getValue
-// @grant			GM_setValue
-// @grant			GM_addStyle
-// @grant			GM_xmlhttpRequest
-// @include			http://weibo.com/*
-// @include			http://www.weibo.com/*
-// @updateURL		https://userscripts.org/scripts/source/114087.meta.js
-// @downloadURL		https://userscripts.org/scripts/source/114087.user.js
-// ==/UserScript==
-
-// 注意：使用@match替换@include将使GM_xmlhttpRequest()失效
-
 (function () {
 
 // 工具函数
