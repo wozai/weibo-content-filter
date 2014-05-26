3398db64f90b52770acae863138c630d331e4cd2
Fang Zhang <zhangfang.thu@gmail.com>
2014-05-26 18:14:34 -0500

    ????????? ??????????? ??Chrome??????

--- head.js
+++ head.js
@@ -14,6 +14,6 @@
 // @grant			unsafeWindow
 // @include			http://weibo.com/*
 // @include			http://www.weibo.com/*
-// @updateURL		https://userscripts.org/scripts/source/114087.meta.js
-// @downloadURL		https://userscripts.org/scripts/source/114087.user.js
+// @updateURL		https://greasyfork.org/scripts/1708/code/%E7%9C%BC%E4%B8%8D%E8%A7%81%E5%BF%83%E4%B8%8D%E7%83%A6%EF%BC%88%E6%96%B0%E6%B5%AA%E5%BE%AE%E5%8D%9A%EF%BC%89.meta.js
+// @downloadURL		https://greasyfork.org/scripts/1708/code/%E7%9C%BC%E4%B8%8D%E8%A7%81%E5%BF%83%E4%B8%8D%E7%83%A6%EF%BC%88%E6%96%B0%E6%B5%AA%E5%BE%AE%E5%8D%9A%EF%BC%89.user.js
 // ==/UserScript==
